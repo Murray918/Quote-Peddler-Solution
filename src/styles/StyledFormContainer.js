e3fb2cea94df4a44f175345938193e09c33f87c3
Andrew Murray <darbyguitars@gmail.com>
2020-01-12 20:34:03 -0600

    styles touch up
    	modified:   src/styles/StyledFormContainer.js

--- src/styles/StyledFormContainer.js
+++ src/styles/StyledFormContainer.js
@@ -4,15 +4,16 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 const StyledPaperWrapper = styled(Paper)`
-@media screen and (min-width: 800px){
-    max-height: 70vh;
-}
-max-height: 53vh;
-    
+	@media screen and (min-width: 800px) {
+		max-height: 70vh;
+	}
+	max-height: 53vh;
 `
 
 export default function StyledPaper({ children, elevation }) {
-	return <StyledPaperWrapper elevation={elevation}>{children}</StyledPaperWrapper>
+	return (
+		<StyledPaperWrapper elevation={elevation}>{children}</StyledPaperWrapper>
+	)
 }
 
 StyledPaper.propTypes = {
